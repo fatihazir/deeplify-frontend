3f97f273bf6948ec646bb3ba1bb6f177ebdadd9a
Fatih HAZIR <fatihhazirceng@gmail.com>
2024-07-17 21:26:53 +0200

    working frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { SharedContextProvider } from './store/context/SharedContext';
+import { GlobalLoadingContextProvider } from './store/context/GlobalLoadingContext';
+import { OverlayContextProvider } from './store/context/OverlayContext';
+import Header from './components/header/Header';
+import Overlay from './components/overlay/Overlay';
+import GlobalLoading from './components/globalLoading/GlobalLoading';
+import Footer from './components/footer/Footer';
+import Container from './components/container/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OverlayContextProvider>
+      <GlobalLoadingContextProvider>
+        <SharedContextProvider>
+          <Header />
+          <Container />
+          <Overlay />
+          <GlobalLoading />
+          <Footer />
+        </SharedContextProvider>
+      </GlobalLoadingContextProvider>
+    </OverlayContextProvider>
   );
 }
 
